ec59803c835fa830630b1e76dd3dd70fd8da3e86
Nick Lin <hawhaw.ya@gmail.com>
2018-08-05 20:25:37 +0800

    添加 AppStoreAPI

---
+++ MoyaTest/MoyaTest/API/AppStoreAPI.swift
@@ -0,0 +1,33 @@
+//
+//  AppStoreAPI.swift
+//  MoyaTest
+//
+//  Created by Nick Lin on 2018/8/5.
+//  Copyright © 2018年 Nick Lin. All rights reserved.
+//
+
+import Moya
+
+enum AppStoreAPI {
+    case getAppVersion(id: String)
+}
+
+extension AppStoreAPI: TargetType {
+    var baseURL: URL { return URL(string: "http://itunes.apple.com")! }
+    var path: String { return "/lookup" }
+    var method: Moya.Method { return .get }
+    var sampleData: Data { return Data() }
+
+    var task: Task {
+        var para: [String: Any] = [:]
+        switch self {
+        case .getAppVersion(let id):
+            para["id"] = id
+        }
+        return .requestParameters(parameters: para, encoding: URLEncoding.default)
+    }
+
+    var headers: [String: String]? { return nil }
+    var validationType: ValidationType { return .successCodes}
+
+}
