4d7cf6f40cb722e141be19be1929ad25c7d5b61f
Nick Lin <hawhaw.ya@gmail.com>
2018-08-07 07:50:02 +0800

    添加主畫面的 UI

--- MoyaTest/MoyaTest/ViewController/MainVC.swift
+++ MoyaTest/MoyaTest/ViewController/MainVC.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 
-class MainVC: UIViewController {
+final class MainVC: UIViewController {
+
+    private lazy var zipButton: UIButton = buttonClosure("郵遞區號")
+    private lazy var libraryButton: UIButton = buttonClosure("圖書館")
+    private lazy var sightseeingFactoryButton: UIButton = buttonClosure("觀光工廠")
+
+    private lazy var stackView: UIStackView = {
+        let stackView = UIStackView(frame: .zero)
+        stackView.axis = .vertical
+        stackView.spacing = 10
+        stackView.alignment = .fill
+        stackView.distribution = .fillEqually
+        return stackView
+    }()
+
+    private lazy var buttonClosure: ( (String) -> UIButton) = { title in
+        let button = UIButton()
+        button.backgroundColor = .red
+        button.setTitle( title, for: .normal)
+        button.addTarget(self, action: #selector(self.didTap(_:)), for: .touchUpInside)
+        return button
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        stackView.addArrangedSubview(zipButton)
+        stackView.addArrangedSubview(libraryButton)
+        stackView.addArrangedSubview(sightseeingFactoryButton)
 
+        view.addSubview(stackView)
+        stackView.mLay(pin: .init(top: 100, left: 20, bottom: 60, right: 20))
+    }
+
+    @objc private func didTap(_ button: UIButton) {
+        switch button {
+        case zipButton:
+            print(button)
+        case libraryButton:
+            print(button)
+        case sightseeingFactoryButton:
+            print(button)
+        default:
+            break
+        }
     }
 
 }
